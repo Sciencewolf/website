2ef69416cfbc364a530628a471f57965a3aa5ece
Sciencewolf <aron17marton@gmail.com>
2022-11-26 13:57:33 +0100

    Create img tags and add src's

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-const changeImages = (count) => {
-    let arrayOfPics = ["download.png", "download2.png", "download3.png"]
-    let imgTag = document.createElement('img')
-    imgTag.src = arrayOfPics[count]
+let count = 0
 
-    imgTag.id = "Pic 1"
-    let getEl = document.getElementById('main')
-    getEl.appendChild(imgTag)
+const arr = new Array(3);
+    arr[0] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download.png"
+    arr[1] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download2.png"
+    arr[2] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download3.png"
 
-    // id.innerHTML = `<img src="${arrayOfPics[count]} alt="Pic1"`
-    
+
+const changeImages = () => {
+    console.log('Begin of func', count)
+
+    if(count > arr.length) {
+        console.log("error: RangeError ")
+
+        count = 0
+        return
+    }
+
+    const getBtnID = document.getElementById('btn')
+    const getClassName = document.querySelector('.main')
+    let imgTag = document.getElementById('pic')
+
+    getBtnID.addEventListener("click", function() {
+        let str = `${arr[count]}`
+        console.log(`Before ${str}`, count)
+        imgTag.src = str.replace(`${arr[count]}`, `${arr[count + 1]}`)
+        console.log(`After ${str}`, count)
+        count++;
+    });
 }
