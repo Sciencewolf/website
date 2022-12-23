0b13433e2377f2691a3e18d0192a121b76b958f7
Sciencewolf <aron17marton@gmail.com>
2022-12-23 17:34:33 +0100

    Transpiled code from ts file

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-let count = 0
-
-const arr = new Array(3);
-    arr[0] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download.png"
-    arr[1] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download2.png"
-    arr[2] = "https://raw.githubusercontent.com/Sciencewolf/website/master/download3.png"
-
-
-const changeImages = () => {
-    console.log('Begin of func', count)
-
-    if(count > arr.length) {
-        console.log("error: RangeError ")
-
-        count = 0
-        return
-    }
-
-    const getBtnID = document.getElementById('btn')
-    const getClassName = document.querySelector('.main')
-    let imgTag = document.getElementById('pic')
-
-    getBtnID.addEventListener("click", function() {
-        let str = `${arr[count]}`
-        console.log(`Before ${str}`, count)
-        imgTag.src = str.replace(`${arr[count]}`, `${arr[count + 1]}`)
-        console.log(`After ${str}`, count)
+"use strict";
+let arr = [
+    "https://raw.githubusercontent.com/Sciencewolf/website/master/download.png",
+    "https://raw.githubusercontent.com/Sciencewolf/website/master/download2.png",
+    "https://raw.githubusercontent.com/Sciencewolf/website/master/download3.png"
+];
+let count = 1;
+const getImages = () => {
+    const getButton = document.getElementById('btn');
+    let getImg = document.getElementById('pic');
+    console.log('getImages', count);
+    getButton.onclick = () => {
+        if (count >= arr.length) {
+            console.log("Error: Out of size");
+            count = 0;
+        }
+        let source = `${arr[count]}`;
+        console.log("Array", source);
+        console.log(getImg, "Current src");
+        if (getImg !== null) {
+            getImg.src = source;
+        }
         count++;
-    });
-}
+    };
+};
